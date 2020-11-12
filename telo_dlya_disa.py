67a77d8d337075401a998d4ca47ffa76f27ca233
Gas <liquidgas.911@gmail.com>
2020-11-12 09:24:32 +0600

    bot

--- telo_dlya_disa.py
+++ telo_dlya_disa.py
@@ -4,7 +4,7 @@ from discord.ext.commands import Bot
 import pyttsx3 
 engine=pyttsx3.init()
 voices = engine.getProperty('voices') 
-engine.setProperty('voice', voices[4].id)
+engine.setProperty('voice', voices['''VOICE ID'''].id)
 rate = engine.getProperty('rate')
 engine.setProperty('rate', 120)
 volume = engine.getProperty('volume')
@@ -12,7 +12,7 @@ engine.setProperty('volume',1.4)
 intents = discord.Intents.all()
 intents.members = True
 client = commands.Bot(command_prefix='.', intents=intents, fetch_offline_members =True)
-kanal=[502490265517293570, 753446436544446515,762570000610623498,762670861597147156]
+kanal=[] # your own text channels ID should be here
 t_chanel=0
 
 
@@ -30,59 +30,23 @@ async def on_ready():
         server = client.get_guild(502490265517293568)
         mess=input("|| ME: ")
         if mess.startswith('!'):
-            if mess=='!osnova':
+            if mess=='!channel_text':
                 t_chanel= server.get_channel(kanal[0])
-                print('Вы на основе')
-            elif mess=='!bio':
-                t_chanel= server.get_channel(kanal[1])
-                print('Вы на био-хим')
-            elif mess=='!code':
-                t_chanel= server.get_channel(kanal[2])
-                print('Вы на коды')
-            elif mess=='!fiz':
-                t_chanel= server.get_channel(kanal[3])
-                print('Вы на физе')
-            elif mess=='!durka':
-                v_channel= server.get_channel(765403207810678785)
+                print('Вы на "channel_text"')
+            elif mess=='!channel_voice':
+                v_channel= server.get_channel('''YOUR CHANNEL ID''')
                 if r==True:
-                    print('Выхожу из "Дурка"')
+                    print('Выхожу из "channel_voice"')
                     r=False
                     await vc.disconnect()
                 else:
-                    print('Захожу в "Дурка"')
+                    print('Захожу в "channel_voice"')
                     vc=await v_channel.connect(timeout=9999.0)
                     r=True
-            elif mess=='!apex':
-                v_channel= server.get_channel(764861667791863868)
-                if r==True:
-                    print('Выхожу из "Apex"')
-                    r=False
-                    await vc.disconnect()
-                else:
-                    print('Захожу в "Apex"')
-                    vc= await v_channel.connect(timeout=9999.0)
-                    r=True
-            elif mess=='!cs':
-                v_channel= server.get_channel(750898887425917038)
-                if r==True:
-                    print('Выхожу из "Контер-Стрике"')
-                    r=False
-                    await vc.disconnect()
-                else:
-                    print('Захожу в "Контер-Стрике"')
-                    vc= await v_channel.connect(timeout=9999.0)
-                    r=True
-            elif mess=='!budlo':
-                v_channel= server.get_channel(705292314997686312)
-                if r==True:
-                    print('Выхожу из "Быдлокодеры"')
-                    r=False
-                    await vc.disconnect()
-                else:
-                    print('Захожу в "Быдлокодеры"')
-                    vc=await v_channel.connect(timeout=9999.0)
-                    r=True
-        elif (mess.startswith('?')) and (r==True):
+                    
+        '''===You can add even more(if and elses) or remove if you don't have so much channels'''
+        
+        if (mess.startswith('?')) and (r==True):
             engine.save_to_file(mess, f'war{i}.mp3')
             engine.runAndWait()
             engine.stop()
@@ -98,8 +62,6 @@ async def on_ready():
 @client.listen('on_message')
 async def message(message):
     channel= message.channel
-    if message.author.id== 771755859428376626:
-        print("||Бот че-то писанул")
     if message.author.id!=774134169953108010:
         print(f'||{message.author}: {message.content}   |{channel}')
 
@@ -107,4 +69,8 @@ async def message(message):
             
     
 
-client.run('Nzc0MTM0MTY5OTUzMTA4MDEw.X6TWoA.Hd3iqXqZSYVIcT7VFqe0CSkY6n0')
+client.run('token')
+
+'''COMMANDS: !text- to get in channel(text and voice both)
+             ?text- bot says "text" in  voice channel
+    "vc is not defined"-error is ok, code will work fine, there should be another one, but without channels ID in kanal it will not raise.'''
